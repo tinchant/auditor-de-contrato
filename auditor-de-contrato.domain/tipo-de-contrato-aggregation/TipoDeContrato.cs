2d218d73aed32ef61f5a22e1edd29500b3c4b106
Tinchant <tinchant@outlook.com>
2020-06-05 03:43:28 -0300

    api pronta

---
+++ auditor-de-contrato.domain/tipo-de-contrato-aggregation/TipoDeContrato.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace auditor_de_contrato.domain.tipo_de_contrato_aggregation
+{
+    public class TipoDeContrato
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+    }
+}
---
+++ auditor-de-contrato.infra/tipo-de-contrato-aggregation/TipoDeContratoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace auditor_de_contrato.api.infra.tipo_de_contrato_aggregation
+{
+    public class TipoDeContratoContext : DbContext
+    {
+        public TipoDeContratoContext (DbContextOptions<TipoDeContratoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<auditor_de_contrato.domain.tipo_de_contrato_aggregation.TipoDeContrato> TipoDeContrato { get; set; }
+    }
+}
